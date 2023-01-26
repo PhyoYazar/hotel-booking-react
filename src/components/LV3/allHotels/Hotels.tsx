a7a989e1bee2fb24e9bcfebe99330010c6f0da5d
Phyo Yarzar <phyoyazar592@gmail.com>
2023-01-27 00:03:32 +0630

    add eslint rules :

--- src/components/LV3/allHotels/Hotels.tsx
+++ src/components/LV3/allHotels/Hotels.tsx
@@ -1,24 +1,21 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import { apiRoutes } from "../../../controllers";
-import useAxios from "../../../hooks/useAxios";
-import { AllHotelsI } from "../../../lib/interface/hotels";
-import HotelCard from "./HotelCard";
+import { apiRoutes } from '../../../controllers';
+import useAxios from '../../../hooks/useAxios';
+import { AllHotelsI } from '../../../lib/interface/hotels';
+import HotelCard from './HotelCard';
 
 const Hotels = () => {
   const { data, isLoading, error } = useAxios<AllHotelsI>({
     endpoint: apiRoutes.getAllHotels,
   });
 
-  if (isLoading) return <div className="flex-center py-32">Loading...</div>;
-  if (error)
-    return (
-      <div className="flex-center py-32">Something went wrong. {error}</div>
-    );
+  if (isLoading) return <div className='flex-center py-32'>Loading...</div>;
+  if (error) return <div className='flex-center py-32'>Something went wrong. {error}</div>;
 
   return (
-    <section className="my-10">
-      <div className="space-y-5">
+    <section className='my-10'>
+      <div className='space-y-5'>
         {data?.data?.map((hotel, index) => (
           <Fragment key={index}>
             <HotelCard hotel={hotel} />
